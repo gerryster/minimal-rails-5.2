f11a070d202c73f2b528e83e794388f18e3be365
Ryan Gerry <rgerry@bluemarblepayroll.com>
2019-11-27 10:47:57 -0600

    Demonstrate bug where asset_path called from a controller becomes stale
    The only differnece here is that asset_path is called from ActionController::Base.helpers from inside a controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_top_level_css
+
+  private
+
+  def set_top_level_css
+    @top_level = ActionController::Base.helpers.asset_path('top_level.css')
+  end
 end
